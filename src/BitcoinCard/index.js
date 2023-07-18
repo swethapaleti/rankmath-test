8c1aa936f356f0b844d52a7ea2968afc6192ab52
Swetha Paleti <swethapaleti555@gmail.com>
2023-07-18 12:10:07 +0200

    add bitcoin card

--- src/BitcoinCard/index.js
+++ src/BitcoinCard/index.js
@@ -1,7 +1,52 @@
-const BitcoinCard = ()=>(
-    <div className="p-4 bg-white rounded-lg">
+import { motion, AnimatePresence } from "framer-motion";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
 
+const BitcoinCard = () => {
+  const [isArrowDown, showInfo] = useState(false);
+
+  return (
+    <div className="w-5/6 min-w-[300px] px-4 py-2 m-auto mt-4 bg-white rounded-lg shadow-md md:mx-4 md:w-1/2 md:px-8">
+      <div className="flex justify-between w-full place-items-center">
+        <div className="flex space-x-8 place-items-center">
+          <img className="w-16" src="bitcoin.svg" alt="bitcoin" />
+          <p className="text-xl">Bitcoin</p>
+        </div>
+        <p className="text-xl">BTC</p>
+      </div>
+      <p className="px-2 py-3 text-4xl font-semibold">3.52925090 BTC</p>
+      <div className="flex justify-between px-2 py-3 place-items-center">
+        <p className="text-lg text-gray-400">$19.153 USD</p>
+        <p className="px-2 text-lg text-white bg-red-500 border rounded-2xl">
+          -2.32%
+        </p>
+      </div>
+      <button onClick={() => showInfo(!isArrowDown)} className="block mx-auto">
+        <IoIosArrowDown className="text-2xl text-gray-500" />
+      </button>
+      <AnimatePresence>
+      {isArrowDown && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            ease: "linear",
+            duration: 0.6,
+          }}
+          className="w-1/4 py-2 mx-auto "
+        >
+          <button className="p-2 mb-2 text-white bg-green-600 md:mb-0 md:mr-2 round-lg">
+            Buy BTC
+          </button>
+          <button className="p-2 text-white bg-red-500 round-lg">
+            Sell BTC
+          </button>
+        </motion.div>
+      )}
+      </AnimatePresence>
     </div>
-)
+  );
+};
 
-export default BitcoinCard
+export default BitcoinCard;
--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,12 +1,18 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { CiMenuKebab } from "react-icons/ci";
 import { MdOutlineArrowBackIosNew } from "react-icons/md";
 import { useState } from "react";
-import {CgEditFlipH} from "react-icons/cg";
-import {GoNote} from "react-icons/go";
-import {LuShare} from "react-icons/lu";
-import {IoMdRemoveCircleOutline} from "react-icons/io"
+import { CgEditFlipH } from "react-icons/cg";
+import { GoNote } from "react-icons/go";
+import { LuShare } from "react-icons/lu";
+import { IoMdRemoveCircleOutline } from "react-icons/io";
 
-const listofMenuItems = [{text:"Edit",icon:CgEditFlipH},{text:"Courier Info",icon:GoNote},{text:"Share",icon:LuShare},{text:"Remove",icon:IoMdRemoveCircleOutline}]
+const listofMenuItems = [
+  { text: "Edit", icon: CgEditFlipH },
+  { text: "Courier Info", icon: GoNote },
+  { text: "Share", icon: LuShare },
+  { text: "Remove", icon: IoMdRemoveCircleOutline },
+];
 
 const Navbar = () => {
   const [isMenuOpen, toggleMenu] = useState(false);
@@ -18,20 +24,35 @@ const Navbar = () => {
           Bitcoin Wallet
         </p>
         <button onClick={() => toggleMenu(!isMenuOpen)}>
-          <CiMenuKebab className="text-3xl transition duration-300 ease-out"/>
+          <CiMenuKebab className="text-3xl" />
         </button>
-      {isMenuOpen && (
-        <div className="absolute flex flex-col right-3 top-20 place-items-end">
-          <div className="w-48 px-4 pt-2 pb-3 space-y-1 bg-white border rounded-lg">
-           {listofMenuItems.map(each=>(
-            <div key={each.text} className="flex justify-between py-2 text-base font-medium transition-colors border-b border-gray-300 cursor-pointer last:border-none place-items-center selection:px-3 hover:text-red-500 hover:font-bold hover:duration-700">
-            <p>{each.text}</p>
-            {<each.icon className="text-xl font-bold"/>}
-          </div>)
-           )}
-          </div>
-        </div>
-      )}
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                ease: "linear",
+                duration: 0.5,
+              }}
+              
+              className="absolute flex flex-col right-3 top-20 place-items-end"
+            >
+              <div className="w-48 px-4 pt-2 pb-3 space-y-1 bg-white border rounded-lg">
+                {listofMenuItems.map((each) => (
+                  <div
+                    key={each.text}
+                    className="flex justify-between py-2 text-base font-medium transition-colors border-b border-gray-300 cursor-pointer last:border-none place-items-center selection:px-3 hover:text-red-500 hover:font-bold hover:duration-700"
+                  >
+                    <p>{each.text}</p>
+                    {<each.icon className="text-xl font-bold" />}
+                  </div>
+                ))}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </>
   );
